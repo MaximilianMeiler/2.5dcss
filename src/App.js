a53b12d044c0152a0793fb9944d1ab587ec75c29
MaximilianMeiler <maximilianmeiler@ufl.edu>
2023-12-24 15:58:59 -0500

    grid

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import './App.css';
 import Block from './Block';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 
 function App() {
   const [xWidth, setXWidth] = useState(20)
-  const [yWidth, setYWidth] = useState(20)
-  const [height, setHeight] = useState(20)
+  const [yWidth, setYWidth] = useState(40)
+  const [height, setHeight] = useState(60)
+  const [xCount, setXCount] = useState(3);
+  const [yCount, setYCount] = useState(3);
 
   return (
     <div>
+
+      <div className='area'>
+        {[...Array(xCount)].map((v, i) => {
+          return [...Array(yCount)].map((w, j) => {
+            return (
+              <Block xWidth={xWidth} yWidth={yWidth} height={1 + height*( i + j)} xPos={805 - i*xWidth*Math.cos(22.5 / 180 * Math.PI) + j*yWidth*Math.cos(22.5 / 180 * Math.PI)} yPos={805 - i*xWidth*Math.sin(22.5 / 180 * Math.PI) - j*yWidth*Math.sin(22.5 / 180 * Math.PI)} zIndex={-i - j}/>
+            )
+          })
+        })}
+      </div>
+
+
+
+
+
+      
+      
+      
       <div>
         <input type='range' min={5} max={100} defaultValue={xWidth} onChange={(v) => setXWidth(v.target.valueAsNumber)}></input>
         <input type='range' min={5} max={100} defaultValue={yWidth} onChange={(v) => setYWidth(v.target.valueAsNumber)}></input>
         <input type='range' min={5} max={100} defaultValue={height} onChange={(v) => setHeight(v.target.valueAsNumber)}></input>
+        <input type='range' min={5} max={100} defaultValue={xCount} onChange={(v) => setXCount(v.target.valueAsNumber)}></input>
+        <input type='range' min={5} max={100} defaultValue={yCount} onChange={(v) => setYCount(v.target.valueAsNumber)}></input>
       </div>
-
-      <Block xWidth={xWidth} yWidth={yWidth} height={height}/>
     </div>
   );
 }
--- src/Block.js
+++ src/Block.js
@@ -1,11 +1,13 @@
 import './Block.css';
+const s = Math.sqrt(2)/2;
+const t = (Math.sqrt(2) - 1)/2;
 
-export default function Block ({xWidth, yWidth, height}) {
+export default function Block ({xWidth, yWidth, height, xPos, yPos, zIndex}) {
   return (
-    <div class="block" style={{marginTop:200 - height - xWidth*.7049166488 - yWidth*.210}}>
+    <div class="block" style={{marginTop: -height - xWidth*s - yWidth*t, left: xPos, top: yPos, zIndex:zIndex}}>
       <figure class="top" style={{width:yWidth, height:xWidth, marginLeft:-1*xWidth/2}}></figure>
-      <figure class="left" style={{width:xWidth, height:height, marginLeft:-1*xWidth, marginTop:xWidth*(.494916649) + (yWidth)*(.210)}}></figure>
-      <figure class="right" style={{width:yWidth, height:height, marginLeft:0, marginTop:xWidth*(.7049166488)}}></figure>
+      <figure class="left" style={{width:xWidth, height:height, marginLeft:-1*xWidth, marginTop:xWidth*(s - t) + (yWidth)*(t)}}></figure>
+      <figure class="right" style={{width:yWidth, height:height, marginTop:xWidth*(s)}}></figure>
     </div>
   )
 }
